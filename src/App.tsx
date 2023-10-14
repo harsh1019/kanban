bcba5eabeda7a69abe637118bdd6bdf17b43bddb
harshrulaniya <harshrulaniya2001@gmail.com>
2023-10-15 01:41:23 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+import MainRoutes from "./Routes";
+
+function App() {
+  console.log("client running");
+  return (
+    <div className="app">
+      <div className="app__navigation">
+        <Link to="/">List sort without library</Link>
+        <Link to="/container-sort">Container sort without library</Link>
+        <Link to="/sort-dnd">Sort with ReactDnD</Link>
+      </div>
+      <MainRoutes />
+    </div>
+  );
+}
+
+export default App;
